0410c3fdf1d0aa22ad3f12bbddff1853947709a1
Cristian Camilo Garcia Patiño <criscagarpa@gmail.com>
2025-08-12 09:58:00 -0500

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,20 @@
+import numpy as np 
+ 
+vector1 = np.array([1,2,3])
+vector2 = np.array([4,5,6])
+
+suma = vector1 + vector2
+print("suma de vectores:", suma)
+
+producto = vector1 * vector2
+print("producto de vectores:", producto )
+
+divicion = vector1 / vector2
+print("producto de vectores:", divicion )
+
+resta = vector1 - vector2
+print("producto de vectores:", resta )
+
+potencia = vector1 ** 2
+print("producto de vectores:", potencia )
+
